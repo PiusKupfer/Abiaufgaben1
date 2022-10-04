0b8a5233ca87dac9b864b293795e7ecb79cb6a31
PiusKupfer <PiusKupfer@0-208-10>
2022-10-04 14:32:08 +0200

    ._.

---
+++ Abiaufgaben1/src/Listenknoten.java
@@ -0,0 +1,11 @@
+
+
+public class Listenknoten <Integer> {
+   
+	public Integer daten;
+    	public Listenknoten<Integer> nachfolger;
+    		
+    	public Listenknoten(int daten, Listenknoten<Integer> nachfolger){
+        
+    }
+}
